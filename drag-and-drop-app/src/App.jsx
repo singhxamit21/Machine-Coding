c6902f5ec990be5922cd7ecb54668468e45b74e4
singhxamit21 <singhxamit21@gmail.com>
2024-11-16 21:26:39 +0530

    drag and drop

---
+++ drag-and-drop-app/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import DragAndDrop from './components/DragAndDrop'
+
+const initialData = {
+  "Todo": [
+    "Design UI mockups",
+    "Set up project repository",
+    "Write unit test",
+    "Integrate payment gateway",
+  ],
+  "In Progress": ["Develop authentication flow", "Implement responsive design"],
+  "Completed": [
+    "Set up CI/CD pipeline",
+    "Conduct code reviews",
+    "Deploy initial version to staging",
+  ],
+};
+function App() {
+
+
+  return (
+    <DragAndDrop initialState={initialData}/>
+  )
+}
+
+export default App
